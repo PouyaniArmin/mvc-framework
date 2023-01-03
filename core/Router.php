c1ddf8bfe7345de3e8e86045914d39623020052b
PouyaniArmin <pouyaniarmin@gmial.com>
2023-01-03 14:03:41 +0330

    fix show view dynamic

--- core/Router.php
+++ core/Router.php
@@ -1,41 +1,70 @@
 <?php
+
 namespace App\core;
+
+use App\Core\Application;
+use App\Core\Response;
+
 /**
  * Class Router
  */
 class Router
 {
-    protected array $routes=[];
+    protected array $routes = [];
     protected Request $request;
-    public function __construct(Request $request)
+    protected Response $response;
+    public function __construct(Request $request, Response $response)
     {
-        $this->request=$request;
+        $this->request = $request;
+        $this->response = $response;
     }
-    public function get($path,$callback){
-        $this->routes['get'][$path]=$callback;
+    public function get($path, $callback)
+    {
+        $this->routes['get'][$path] = $callback;
     }
-    public function post($path,$callback){
-        $this->routes['post'][$path]=$callback;
+    public function post($path, $callback)
+    {
+        $this->routes['post'][$path] = $callback;
     }
 
-    public function resolve(){
-       $path=$this->request->path();
-        $method=$this->request->method();
-        $callback=$this->routes[$method][$path] ?? false;
+    public function resolve()
+    {
+        $path = $this->request->path();
+        $method = $this->request->method();
+        $callback = $this->routes[$method][$path] ?? false;
         if ($callback === false) {
-            return "not Found";
-            exit;
+            $this->response->setStatusCode(404);
+            return $this->renderView('_404');
         }
         if (is_string($callback)) {
-           return $this->renderView($callback);
+            return $this->renderView($callback);
+        }
+        if(is_array($callback)){
+            Application::$app->controller=new $callback[0];
+            $callback[0]=Application::$app->controller;
+        }
+        return call_user_func($callback,$this->request);
     }
-        return call_user_func($callback);
+    public function renderView($view,$parmas=[])
+    {
+        $layoutContent = $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view,$parmas);
+        return str_replace("{{content}}", $viewContent, $layoutContent);
     }
-    public function renderView($view){
-        $laycontent=$this->layoutContent();
-        include_once "../views/$view.php";        
+    protected function layoutContent()
+    {
+        $layout=Application::$app->controller->layout;
+        ob_start();
+        include_once Application::$ROOT_DIR . "/views/layouts/$layout.php";
+        return ob_get_clean();
     }
-    protected function layoutContent(){
-        include_once __DIR__."../views/layouts/main.php";
+    protected function renderOnlyView($view,$parmas)
+    {
+        foreach($parmas as $key=>$value){
+            $$key=$value;
+        }
+        ob_start();
+        include_once Application::$ROOT_DIR . "/views/$view.php";
+        return ob_get_clean();
     }
 }
