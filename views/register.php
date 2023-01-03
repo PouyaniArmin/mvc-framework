25a7b7fedf55001c2e001d59a57f0abfc5ce93cd
PouyaniArmin <pouyaniarmin@gmial.com>
2023-01-03 13:59:44 +0330

    register page

---
+++ views/register.php
@@ -0,0 +1,38 @@
+<div class="p-2">
+<h1>Register </h1>
+<div class="p-3">
+<form action="" method="post">
+<div class="row">
+    <div class="col">
+
+<div class="mb-3">
+        <label class="form-label">FirstName</label>
+        <input type="text" name="firstname" class="form-control" >
+    </div>
+    </div>
+    <div class="col">
+    <div class="mb-3">
+        <label class="form-label">LastName</label>
+        <input type="text" name="lastname" class="form-control" >
+    </div>
+    </div>
+</div>
+    
+    <div class="mb-3">
+        <label class="form-label">Email address</label>
+        <input type="email" name="email" class="form-control" >
+    </div>
+
+    <div class="mb-3">
+        <label class="form-label">Password</label>
+        <input type="password" name="pass" class="form-control" >
+    </div>
+
+    <div class="mb-3">
+        <label class="form-label">Confirm Password</label>
+        <input type="password" name="confirmPass" class="form-control" >
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+</div>
