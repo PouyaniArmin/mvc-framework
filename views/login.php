9318c4c16f511189003f311727da7bc373b8f95c
PouyaniArmin <pouyaniarmin@gmial.com>
2023-01-08 21:30:23 +0330

    login forn

--- views/login.php
+++ views/login.php
@@ -1,1 +1,14 @@
-<h1>Login</h1>
+<?php 
+/**
+ * @var $model App\User */
+?>
+<div class="p-2">
+    <h1>Login</h1>
+    <div class="p-3">
+        <?php $form= \App\Core\Form\Form::begin('',"post"); ?>
+            <?php echo $form->field($model,'email')->emailField() ?>
+            <?php echo $form->field($model,'password')->passwordField() ?>    
+            <button type="submit" class="btn btn-primary">Submit</button>
+            <?php echo \App\Core\Form\Form::end(); ?>
+    </div>
+</div>
