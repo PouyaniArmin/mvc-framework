68a3ecb65b0aa34a7ba5fb69b07ecd83a07bfbe5
PouyaniArmin <pouyaniarmin@gmial.com>
2023-01-08 21:31:36 +0330

    index.

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 use App\Controllers\AuthController;
 use App\Controllers\SiteController;
 use App\Core\Application;
+use App\Models\User;
 use Dotenv\Dotenv;
 
 require_once __DIR__."/../vendor/autoload.php";
@@ -14,6 +15,8 @@ $db_config=[
     'user'=>$_ENV['DB_USER'],
     'password'=>$_ENV['DB_PASSWORD']
 ];
+
+
 $app= new Application(dirname(__DIR__),$db_config);
 
 
@@ -25,4 +28,5 @@ $app->router->get('/login',[AuthController::class,'login']);
 $app->router->post('/login',[AuthController::class,'login']);
 $app->router->get('/register',[AuthController::class,'register']);
 $app->router->post('/register',[AuthController::class,'register']);
+$app->router->get('/logout',[AuthController::class,'logout']);
 $app->run();
