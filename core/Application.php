d1ab8c21c0f06e19fe72abe03a99ddf5845771ea
PouyaniArmin <pouyaniarmin@gmial.com>
2023-01-08 21:38:14 +0330

    object user add login and logout

--- core/Application.php
+++ core/Application.php
@@ -4,6 +4,7 @@ namespace App\Core;
 
 use App\core\Request;
 use App\core\Router;
+use App\Models\User;
 
 /**
  * class Application
@@ -13,6 +14,7 @@ use App\core\Router;
 class Application
 {
     public static string $ROOT_DIR;
+    public string $userClass;
     public Router $router;
     public Request $request;
     public Response $response;
@@ -20,25 +22,59 @@ class Application
     public Database $db;
     public static Application $app;
     public Controller $controller;
-    public function __construct($rootPath,array $db_config)
+    public ?DbModel $user;
+
+    public function __construct($rootPath, array $db_config)
     {
-        self::$ROOT_DIR=$rootPath;
-        self::$app=$this;
-        $this->request=new Request;
-        $this->response=new Response;
-        $this->session=new Session();
-        $this->router=new Router($this->request,$this->response);
-        $this->db=new Database($db_config);
+        self::$ROOT_DIR = $rootPath;
+        self::$app = $this;
+        $this->request = new Request;
+        $this->response = new Response;
+        $this->session = new Session();
+        $this->router = new Router($this->request, $this->response);
+        $this->db = new Database($db_config);
+
+        $this->user = new User;
+
+        $primaryValue = $this->session->get('user');
+        if ($primaryValue) {
+            $primaryKey = $this->user->primaryKey();
+            $this->user = $this->user->findOne([$primaryKey => $primaryValue]);
+        }else{
+            $this->user=null;   
+        }
     }
 
-    public function getController():Controller{
+    public function getController(): Controller
+    {
         return $this->controller;
     }
-    public function seController(Controller $controller){
-        $this->controller=$controller;
+    public function seController(Controller $controller)
+    {
+        $this->controller = $controller;
     }
 
-    public function run(){
-       echo $this->router->resolve();
+    public function login(DbModel $user)
+    {
+        $this->user = $user;
+        $primaryKey = $user->primaryKey();
+        $primaryValue = $user->{$primaryKey};
+        $this->session->set('user', $primaryValue);
+        return true;
+    }
+
+    public function logout()
+    {
+        $this->user = null;
+        $this->session->remove('user');
+    }
+
+    public static function isGuest(){
+        return !self::$app->user;
+    }
+
+    public function run()
+    {
+        echo $this->router->resolve();
     }
 }
