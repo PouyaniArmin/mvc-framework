b502328eb7f85a3deeef244a2499242e84b1b612
PouyaniArmin <pouyaniarmin@gmial.com>
2023-01-09 17:59:54 +0330

    create form with php code

--- views/contact.php
+++ views/contact.php
@@ -1,17 +1,18 @@
+<?php
 
+/** @var App\Core\View $this*/
+/** @var App\Models\ContactForm $model*/
+
+use App\Core\Form\Form;
+use App\Core\Form\TextareaField;
+
+$this->title = 'Contact';
+?>
 <h1>Contact</h1>
-<form action="" method="post">
-<div class="mb-3">
-        <label class="form-label">Subject</label>
-        <input type="text" name="subject" class="form-control" >
-    </div>
-    <div class="mb-3">
-        <label class="form-label">Email address</label>
-        <input type="email" name="email" class="form-control" >
-    </div>
-    <div class="mb-3">
-        <label class="form-label">Body</label>
-        <textarea name="body" rows="5" class="form-control"></textarea>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+
+<?php $form = Form::begin('', 'post') ?>
+<?php echo $form->field($model, 'subject') ?>
+<?php echo $form->field($model, 'email') ?>
+<?php echo new TextareaField($model, 'body') ?>
+<button type="submit" class="btn btn-primary">Submit</button>
+<?php Form::end(); ?>
