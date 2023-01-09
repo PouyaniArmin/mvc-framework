34d0ce5ec24e4a970f83188440be60e44f664d18
PouyaniArmin <pouyaniarmin@gmial.com>
2023-01-09 17:52:35 +0330

    add contact form

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -4,18 +4,24 @@
 use App\Core\Application;
 use App\Core\Controller;
 use App\core\Request;
+use App\Core\Response;
+use App\Models\ContactForm;
 
  class SiteController extends Controller{
     public function home(){
         $params=['name'=>'Armin'];
         return $this->render('home',$params);
     }
-    public function contact(){
-        return $this->render('contact');
-    }
-    public function index(Request $request){
-        $body=$request->body();
-        var_dump($body);
-        return $this->render('contact');
+    public function contact(Request $request,Response $response){
+        $contact=new ContactForm;
+        if ($request->isPost()) {
+            $contact->loadData($request->body());
+            if ($contact->validate() && $contact->send()) {
+                Application::$app->session->setFlash('success','Thanks for contacing us');
+                return $response->redirect('contact');
+            }
+
+        }
+        return $this->render('contact',['model'=>$contact]);
     }
  }
