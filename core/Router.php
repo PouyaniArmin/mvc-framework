e5f2fd751505e4a2ebec271d37daa11b22c1ca4b
PouyaniArmin <pouyaniarmin@gmial.com>
2023-01-09 17:56:18 +0330

    ->view call class view for render

--- core/Router.php
+++ core/Router.php
@@ -12,6 +12,7 @@ use App\Core\Response;
  */
 class Router
 {
+
     protected array $routes = [];
     protected Request $request;
     protected Response $response;
@@ -38,7 +39,7 @@ class Router
             throw new NotFoundException();
         }
         if (is_string($callback)) {
-            return $this->renderView($callback);
+            return Application::$app->view->renderView($callback);
         }
         if (is_array($callback)) {
             /** @var App\Core\Controller $contrpller */
@@ -53,30 +54,5 @@ class Router
         }
         return call_user_func($callback, $this->request, $this->response);
     }
-    public function renderView($view, $parmas = [])
-    {
-        $layoutContent = $this->layoutContent();
-        $viewContent = $this->renderOnlyView($view, $parmas);
-        return str_replace("{{content}}", $viewContent, $layoutContent);
-    }
-    protected function layoutContent()
-    {
-
-        $layout = Application::$app->layout;
-        if (Application::$app->controller) {
-            $layout = Application::$app->controller->layout;
-        }
-        ob_start();
-        include_once Application::$ROOT_DIR . "/views/layouts/$layout.php";
-        return ob_get_clean();
-    }
-    protected function renderOnlyView($view, $parmas)
-    {
-        foreach ($parmas as $key => $value) {
-            $$key = $value;
-        }
-        ob_start();
-        include_once Application::$ROOT_DIR . "/views/$view.php";
-        return ob_get_clean();
-    }
+    
 }
