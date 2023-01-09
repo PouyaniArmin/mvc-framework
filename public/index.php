96c6bc76fe98177b29f58f1f6f5ac41095391f08
PouyaniArmin <pouyaniarmin@gmial.com>
2023-01-09 17:58:03 +0330

    index

--- public/index.php
+++ public/index.php
@@ -22,12 +22,11 @@ $app= new Application(dirname(__DIR__),$db_config);
 
 $app->router->get('/',[SiteController::class,'home']);
 $app->router->get('/contact',[SiteController::class,'contact']);
-$app->router->post('/contact',[SiteController::class,'index']);
-
+$app->router->post('/contact',[SiteController::class,'contact']);
 $app->router->get('/login',[AuthController::class,'login']);
 $app->router->post('/login',[AuthController::class,'login']);
 $app->router->get('/register',[AuthController::class,'register']);
 $app->router->post('/register',[AuthController::class,'register']);
-$app->router->get('/profile',[AuthController::class,'profile']);
 $app->router->get('/logout',[AuthController::class,'logout']);
+$app->router->get('/profile',[AuthController::class,'profile']);
 $app->run();
